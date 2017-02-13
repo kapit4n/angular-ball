305ca1042aba984df51111bacf8c9096b292d6cf
larce <luis.arce22@gmail.com>
2017-02-12 21:52:32 -0400

    Updating to get teams related to the championship

--- client/src/app/championship/championship-show/championship-show.component.ts
+++ client/src/app/championship/championship-show/championship-show.component.ts
@@ -1,34 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ChampionshipApi }            from '../../../../sdk/services';
-import { Championship }  from '../../../../sdk/models';
-import {LoadDataInterface} from '../../loadDataInterface'
+import { ChampionshipApi, ChampionshipRowApi, TeamChampionshipRowApi }            from '../../../../sdk/services';
+import { Championship, Team, TeamChampionshipRow, ChampionshipRow }  from '../../../../sdk/models';
+import {LoadDataWithChildrenInterface} from '../../loadDataInterface'
 
 @Component({
   selector: 'app-championship-show',
   templateUrl: './championship-show.component.html',
-  styleUrls: ['./championship-show.component.css'],
-  providers: [ChampionshipApi]
+  styleUrls: ['./championship-show.component.css']
 })
 
-export class ChampionshipShowComponent implements OnInit, LoadDataInterface {
+export class ChampionshipShowComponent implements OnInit, LoadDataWithChildrenInterface {
   data: Championship;
+  current: ChampionshipRow;
+  children: Team[];
   id: any;
   paramsSub: any;
 
-  constructor(private activatedRoute: ActivatedRoute, private dataApi : ChampionshipApi) {
-    this.data = {id: 0, name: "Default Name", logoUrl: "test.png", description: "Description"};
+  constructor(private activatedRoute: ActivatedRoute, private dataApi: ChampionshipApi,
+              private childrenApi: TeamChampionshipRowApi, private currentRowApi: ChampionshipRowApi) {
+    this.data = {id: 0, name: "", logoUrl: "", description: ""};
+    this.children = [];
     this.paramsSub = this.activatedRoute.params.subscribe(params => { 
         this.id = parseInt(params['id'], 10);
         this.loadData(this.id);
+        this.loadChildren(this.id);
       }
     );
   }
 
   loadData(id: any): void {
-      this.dataApi.findById(id).subscribe((data: Championship) => {
-        this.data = data;
+    this.dataApi.findById(id).subscribe((data: Championship) => {
+      this.data = data;
+      this.currentRowApi.find({include: {relation:'championship', scope: { where: { id: id }}} }).subscribe((rows: ChampionshipRow[]) => {
+        if(rows.length > 0) {
+          this.current = rows[0];
+          this.childrenApi.find({include: [{relation:'championshipRow', scope: {where: {id: this.current.id}}},'team']}).subscribe((teams: TeamChampionshipRow[]) => {
+            teams.forEach(function(row) {
+              this.children.push(row.team);// See why is not rendering in the page
+            });
+          });
+        }
       });
+    });
+  }
+
+  loadChildren(id: any): void {
+    this.childrenApi.find({}).subscribe((data: TeamChampionshipRow[]) => {
+    });
   }
 
   ngOnInit() {
--- client/src/app/loadDataInterface.ts
+++ client/src/app/loadDataInterface.ts
@@ -2,3 +2,8 @@
 export interface LoadDataInterface {
     loadData(id: any);
 }
+
+export interface LoadDataWithChildrenInterface {
+    loadData(id: any);
+    loadChildren(id: any);
+}
