e97045a56bb7415908cfbb16b124d1b12350fff2
larce <luis.arce22@gmail.com>
2017-03-24 09:42:56 -0400

    Clear match components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -19,10 +19,6 @@ import { ChampionshipAddComponent } from './championship/championship-add/champi
 import { ChampionshipListComponent } from './championship/championship-list/championship-list.component';
 import { TeamComponent } from './team/team/team.component';
 import { ChampionshipComponent } from './championship/championship/championship.component';
-import { MatchAddComponent } from './match/match-add/match-add.component';
-import { MatchEditComponent } from './match/match-edit/match-edit.component';
-import { MatchListComponent } from './match/match-list/match-list.component';
-import { MatchShowComponent } from './match/match-show/match-show.component';
 
 const appRoutes: Routes = [
   { path: '', component: AppComponent },
@@ -54,7 +50,7 @@ const appRoutes: Routes = [
     TeamEditComponent, TeamAddComponent,
     TeamShowComponent, TeamEditComponent,
     TeamAddComponent, TeamListComponent,
-    TeamShowComponent, PageNotFoundComponent, ChampionshipShowComponent, ChampionshipEditComponent, ChampionshipAddComponent, ChampionshipListComponent, TeamComponent, ChampionshipComponent, MatchAddComponent, MatchEditComponent, MatchListComponent, MatchShowComponent
+    TeamShowComponent, PageNotFoundComponent, ChampionshipShowComponent, ChampionshipEditComponent, ChampionshipAddComponent, ChampionshipListComponent, TeamComponent, ChampionshipComponent
   ],
   imports: [
     BrowserModule,
