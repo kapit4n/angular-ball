f99d17c82613fb374f28a7145898281876a7b31d
larce <luis.arce22@gmail.com>
2017-04-25 18:11:01 -0400

    Added team service to work with server RESTFULL requests

---
+++ client/src/app/services/teamService.ts
@@ -0,0 +1,55 @@
+import { Injectable }              from '@angular/core';
+import { Http, Response, Headers, RequestOptions  }          from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+import { Championship, Team, TeamChampionshipRow, ChampionshipRow }  from '../../../sdk/models';
+
+@Injectable()
+export class TeamService {
+  private teamsUrl = 'http://localhost:3000/api/teams';  // URL to web API
+  
+  constructor (private http: Http) {}
+
+  getTeams(): Observable<Team[]> {
+    return this.http.get(this.teamsUrl)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  getTeamById(id: any): Observable<Team> {
+    return this.http.get(this.teamsUrl + "/" + id)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    console.log(body);
+    return body || [];
+  }
+
+  create(name: string): Observable<Team> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.teamsUrl, { name }, options)
+                    .map(this.extractData)
+                    .catch(this.handleError);
+  }
+
+  private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
--- client/src/app/team/team-list/team-list.component.ts
+++ client/src/app/team/team-list/team-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TeamApi }            from '../../../../sdk/services';
+import { TeamService }            from '../../services/teamService';
 import { Team }  from '../../../../sdk/models';
 import {LoadListDataInterface} from '../../loadListDataInterface'
 
@@ -7,26 +8,27 @@ import {LoadListDataInterface} from '../../loadListDataInterface'
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
-  styleUrls: ['./team-list.component.css']
+  styleUrls: ['./team-list.component.css'],
+  providers: [TeamService]
 })
 export class TeamListComponent implements OnInit, LoadListDataInterface {
-  data = [];
-
-  constructor(private dataApi: TeamApi) { }
+  data:Team[] = [];
+  errorMessage = ""
+  constructor(private dataApi: TeamService) { }
 
   ngOnInit() {
   	this.loadData();
   }
 
   loadData(): void {
-    this.dataApi.find({}).subscribe((data) => {
+    this.dataApi.getTeams().subscribe((data) => {
       this.data = data;
-    });
+    }, error =>  this.errorMessage = <any>error);
   }
 
   delete(id: any): void {
-    this.dataApi.deleteById(id).subscribe((data) => {
-      this.loadData();
-    });
+    //this.dataApi.deleteById(id).subscribe((data) => {
+    //  this.loadData();
+    //});
   }
 }
