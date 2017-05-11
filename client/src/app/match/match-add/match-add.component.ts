31e2d68a2a587eb86a745aadad03ae0126dea76a
larce <luis.arce22@gmail.com>
2017-05-10 20:20:44 -0400

    Updated match add and match list to work with loopback API

--- client/src/app/match/match-add/match-add.component.ts
+++ client/src/app/match/match-add/match-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatchApi }            from '../../../../sdk/services';
+import { Match }  from '../../../../sdk/models';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-match-add',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./match-add.component.css']
 })
 export class MatchAddComponent implements OnInit {
-
-  constructor() { }
+  data: any;
+  constructor(private dataApi: MatchApi, private router: Router) {
+    this.data = {matchDate: "05/10/2017"};
+  }
 
   ngOnInit() {
+  }
+
+  save(): void  {
+    this.dataApi.create(this.data).subscribe((data: Match) => {
+      this.router.navigate(['match/']);
+    });
   }
 
 }
--- client/src/app/match/match-list/match-list.component.ts
+++ client/src/app/match/match-list/match-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { MatchApi }            from '../../../../sdk/services';
+import { Match }  from '../../../../sdk/models';
+import {LoadListDataInterface} from '../../loadListDataInterface'
 
 @Component({
   selector: 'app-match-list',
   templateUrl: './match-list.component.html',
   styleUrls: ['./match-list.component.css']
 })
-export class MatchListComponent implements OnInit {
-
-  constructor() { }
+export class MatchListComponent implements OnInit, LoadListDataInterface {
+  data:Match[] = [];
+  errorMessage = ""
+  constructor(private dataApi: MatchApi) { }
 
   ngOnInit() {
+  	this.loadData();
   }
 
+  loadData(): void {
+    this.dataApi.find({}).subscribe((data: Match[]) => {
+      this.data = data;
+    });
+  }
+
+  delete(id: any): void {
+    this.dataApi.deleteById(id).subscribe((data) => {
+      this.loadData();
+    });
+  }
 }
