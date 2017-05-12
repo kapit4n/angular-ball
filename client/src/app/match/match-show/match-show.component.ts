111f958bf27db65e0da0417ae60fe78ab7c9ba2b
larce <luis.arce22@gmail.com>
2017-05-11 21:04:22 -0400

    Updated match show to display match information

--- client/src/app/match/match-show/match-show.component.ts
+++ client/src/app/match/match-show/match-show.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { MatchApi }            from '../../../../sdk/services';
+import { Match }  from '../../../../sdk/models';
+import { LoadDataInterface } from '../../loadDataInterface'
 
 @Component({
   selector: 'app-match-show',
   templateUrl: './match-show.component.html',
-  styleUrls: ['./match-show.component.css']
+  styleUrls: ['./match-show.component.css'],
+  providers: [MatchApi]
 })
-export class MatchShowComponent implements OnInit {
+export class MatchShowComponent implements OnInit, LoadDataInterface {
+  data: any;
+  id: any;
+  paramsSub: any;
+  teams = [];
+  constructor(private activatedRoute: ActivatedRoute, private dataApi : MatchApi) {
+    this.data = { name: "RRR", logoUrl: "RRR", description: "Des"};
+    this.paramsSub = this.activatedRoute.params.subscribe(params => { 
+        this.id = params['id'];
+        this.loadData(this.id);
+      }
+    );
+  }
 
-  constructor() { }
+  loadData(id: any): void {
+      this.dataApi.findById(id).subscribe((data: Match) => {
+        this.data = data;
+      });
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
+  }
 }
