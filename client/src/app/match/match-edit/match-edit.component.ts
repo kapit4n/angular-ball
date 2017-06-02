d27584b20fa0a03f0406b766d2b0516ebf687fdb
larce <luis.arce22@gmail.com>
2017-06-01 21:03:13 -0400

    Updated match edit component to save its changes

--- client/src/app/match/match-edit/match-edit.component.ts
+++ client/src/app/match/match-edit/match-edit.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { MatchApi }            from '../../../../sdk/services';
+import { Match }  from '../../../../sdk/models';
+import { LoadDataInterface } from '../../loadDataInterface'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-match-edit',
   templateUrl: './match-edit.component.html',
   styleUrls: ['./match-edit.component.css']
 })
-export class MatchEditComponent implements OnInit {
+export class MatchEditComponent implements OnInit, LoadDataInterface {
 
-  constructor() { }
+  data: any;
+  id: any;
+  paramsSub: any;
+  constructor(private activatedRoute: ActivatedRoute, private dataApi : MatchApi, private router: Router) {
+    this.data = {matchDate: " "};
+    this.paramsSub = this.activatedRoute.params.subscribe(params => { 
+        this.id = params['id'];
+        this.loadData(this.id);
+      }
+    );
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
+  }
+
+  loadData(id: any): void {
+      this.dataApi.findById(id).subscribe((data: Match) => {
+        this.data = data;
+      });
+  }
+
+  save(): void  {
+    this.dataApi.upsert(this.data).subscribe((data: Match) => {
+      this.router.navigate(['match/show/' + data.id]);
+    });
+  }
 }
