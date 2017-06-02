9bd7c108aa05a3a885be25c87762f9d1de706b5f
larce <luis.arce22@gmail.com>
2017-06-01 21:59:09 -0400

    Updated player list page to show all saved data

--- client/src/app/player/player-list/player-list.component.ts
+++ client/src/app/player/player-list/player-list.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerApi }            from '../../../../sdk/services';
+import { Player }  from '../../../../sdk/models';
+import {LoadListDataInterface} from '../../loadListDataInterface'
 
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
   styleUrls: ['./player-list.component.css']
 })
-export class PlayerListComponent implements OnInit {
-
-  constructor() { }
+export class PlayerListComponent implements OnInit, LoadListDataInterface {
+  data:Player[] = [];
+  errorMessage = ""
+  constructor(private dataApi: PlayerApi) { }
 
   ngOnInit() {
+  	this.loadData();
   }
 
+  loadData(): void {
+    this.dataApi.find({}).subscribe((data: Player[]) => {
+      this.data = data;
+    });
+  }
+
+  delete(id: any): void {
+    this.dataApi.deleteById(id).subscribe((data) => {
+      this.loadData();
+    });
+  }
 }
