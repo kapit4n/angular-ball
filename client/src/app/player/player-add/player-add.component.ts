ef4c6dfb69f96c588e0103e01c8213fed6c3600b
larce <luis.arce22@gmail.com>
2017-06-01 21:58:42 -0400

    Updated Player add component to save data in the database

--- client/src/app/player/player-add/player-add.component.ts
+++ client/src/app/player/player-add/player-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PlayerApi }            from '../../../../sdk/services';
+import { Player }  from '../../../../sdk/models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-player-add',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-add.component.css']
 })
 export class PlayerAddComponent implements OnInit {
+  data: any;
+  paramsSub: any;
 
-  constructor() { }
+  constructor(private dataApi: PlayerApi, private router: Router) {
+    this.data = { name: " " };
+  }
 
   ngOnInit() {
+
+  }
+
+  save(): void  {
+    this.dataApi.create(this.data).subscribe((data: Player) => {
+      this.router.navigate(['player/']);
+    });
   }
 
 }
