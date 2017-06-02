e9ec9094ede9b6df0640b7959b4142a2a6c16156
larce <luis.arce22@gmail.com>
2017-06-01 21:59:43 -0400

    Updated player edit component to save edited data

--- client/src/app/player/player-edit/player-edit.component.ts
+++ client/src/app/player/player-edit/player-edit.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { PlayerApi }            from '../../../../sdk/services';
+import { Player }  from '../../../../sdk/models';
+import { LoadDataInterface } from '../../loadDataInterface'
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-player-edit',
   templateUrl: './player-edit.component.html',
   styleUrls: ['./player-edit.component.css']
 })
-export class PlayerEditComponent implements OnInit {
+export class PlayerEditComponent implements OnInit, LoadDataInterface {
 
-  constructor() { }
+  data: any;
+  id: any;
+  paramsSub: any;
+  constructor(private activatedRoute: ActivatedRoute, private dataApi : PlayerApi, private router: Router) {
+    this.data = {name: " ", logoUrl: " ", description: "0"};
+    this.paramsSub = this.activatedRoute.params.subscribe(params => { 
+        this.id = params['id'];
+        this.loadData(this.id);
+      }
+    );
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
+  }
+
+  loadData(id: any): void {
+      this.dataApi.findById(id).subscribe((data: Player) => {
+        this.data = data;
+      });
+  }
+
+  save(): void  {
+    this.dataApi.upsert(this.data).subscribe((data: Player) => {
+      this.router.navigate(['player/show/' + data.id]);
+    });
+  }
 }
