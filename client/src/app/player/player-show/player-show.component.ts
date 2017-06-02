357ec4cd79316d4d90ef58eb1104890351a8136b
larce <luis.arce22@gmail.com>
2017-06-01 22:00:33 -0400

    Updated player show page to display player information saved in the database

--- client/src/app/player/player-show/player-show.component.ts
+++ client/src/app/player/player-show/player-show.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { PlayerApi }            from '../../../../sdk/services';
+import { Player }  from '../../../../sdk/models';
+import { LoadDataInterface } from '../../loadDataInterface'
 
 @Component({
   selector: 'app-player-show',
   templateUrl: './player-show.component.html',
   styleUrls: ['./player-show.component.css']
 })
-export class PlayerShowComponent implements OnInit {
+export class PlayerShowComponent implements OnInit , LoadDataInterface {
+  data: any;
+  id: any;
+  paramsSub: any;
+  teams = [];
+  constructor(private activatedRoute: ActivatedRoute, private dataApi : PlayerApi) {
+    this.data = { name: "", avatarUrl: "", biography: ""};
+    this.paramsSub = this.activatedRoute.params.subscribe(params => { 
+        this.id = params['id'];
+        this.loadData(this.id);
+      }
+    );
+  }
 
-  constructor() { }
+  loadData(id: any): void {
+      this.dataApi.findById(id).subscribe((data: Player) => {
+        this.data = data;
+      });
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
+  }
 }
